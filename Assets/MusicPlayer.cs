169784134273db965e47c75c12df370ffb03e5d8
S&K <sdg4631@gmail.com>
2018-04-11 00:37:30 +0900

    added paper plane and created splash

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+    {
+        Invoke("LoadFirstScene", 10f);
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
