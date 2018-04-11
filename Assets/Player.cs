0adfd850a57c76105ff3c866a7d65688ccd5891c
S&K <sdg4631@gmail.com>
2018-04-11 12:57:16 +0900

    Rotation with Quaternion.Euler

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -20,26 +21,28 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update()
     {
-        HorizontalMovement();
-        VerticalMovement();
+        ProcessTranslation();
+        ProcessRotation();
 
     }
 
-    private void HorizontalMovement()
+    private void ProcessTranslation()
     {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
         transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
-    }
 
-    private void VerticalMovement()
-    {
         float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         float yOffset = yThrow * ySpeed * Time.deltaTime;
         float rawYPos = transform.localPosition.y + yOffset;
         float clampedYPos = Mathf.Clamp(rawYPos, -yRangeDown, yRangeUp);
         transform.localPosition = new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        transform.localRotation = Quaternion.Euler(0f, 360f, 0f);
     }
 }
