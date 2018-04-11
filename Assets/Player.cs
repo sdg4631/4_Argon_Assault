1f3750b839314021ef84c88e5b20b10551e9e38b
S&K <sdg4631@gmail.com>
2018-04-11 12:37:25 +0900

    Added new plane model with better normals and added y movement of our plane

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,8 +5,11 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
-    [Tooltip("In m")] [SerializeField] float xRange = 3.75f;
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 10f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 10f;
+    [Tooltip("In m")] [SerializeField] float xRange = 4f;
+    [Tooltip("In m")] [SerializeField] float yRangeUp = 2.25f;
+    [Tooltip("In m")] [SerializeField] float yRangeDown = 2.5f;
 
     // Use this for initialization
     void Start()
@@ -17,11 +20,26 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");        
-        float xOffset = xThrow * xSpeed * Time.deltaTime;
-        float rawNewXPos = transform.localPosition.x + xOffset;
-        float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
-        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+        HorizontalMovement();
+        VerticalMovement();
 
-	}
+    }
+
+    private void HorizontalMovement()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+
+    private void VerticalMovement()
+    {
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRangeDown, yRangeUp);
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
+    }
 }
